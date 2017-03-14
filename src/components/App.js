b5c080577bf1c9f68ef705398614409b1e824fb4
Ryan Walsh <ryan@thehatrack.net>
2017-03-14 09:48:48 -0600

    found error in master

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,15 @@ import React from "react";
 
 import "./App.css";
 
+import router from "../router";
+
 import Nav from "./Nav/Nav";
 
 export function App( { children } ) {
 	return (
 		<div className="app">
 			<Nav />
+			{ router }
 		</div>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 
@@ -9,8 +10,10 @@ import store from "./store";
 import App from "./components/App";
 
 ReactDOM.render(
-	<Provider store={ store }>
-		<App />
-	</Provider>,
+	<BrowserRouter>
+		<Provider store={ store }>
+			<App />
+		</Provider>
+	</BrowserRouter>,
 	document.getElementById( 'root' )
 );
