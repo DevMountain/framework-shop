b5db94ff8db35875094fb9532cd3058136894a2e
Ryan Walsh <ryan@thehatrack.net>
2017-03-14 09:35:15 -0600

    missed solution code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 
@@ -10,10 +9,8 @@ import store from "./store";
 import App from "./components/App";
 
 ReactDOM.render(
-	<BrowserRouter>
-		<Provider store={ store }>
-			<App />
-		</Provider>
-	</BrowserRouter>,
+	<Provider store={ store }>
+		<App />
+	</Provider>,
 	document.getElementById( 'root' )
 );
